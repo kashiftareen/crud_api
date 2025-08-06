957607530a56612500a2cdede692df8982ecc563
kashiftareen <kashif.tareen1999@gmail.com>
2025-08-06 18:03:54 +0500

    Add database engine setup and DB session dependency using SQLAlchemy

--- fastapi_crud/database.py
+++ fastapi_crud/database.py
@@ -1,1 +1,23 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+#Create engine with connection string
+
+sqlalchemy_database_URL=settings.db_uri
+engine = create_engine(sqlalchemy_database_URL)
+
+# Creates a new SQLAlchemy session factory with specific settings and binds it to the engine.
+localsession =sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+
+#dependancy to make connection with database and close it automatically
+ 
+def get_db():
+    db = localsession()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
