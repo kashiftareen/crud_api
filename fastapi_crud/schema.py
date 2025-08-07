6b9890f3afe103bc72b0f42670995608ac6378a6
kashiftareen <kashif.tareen1999@gmail.com>
2025-08-07 17:45:57 +0500

    add GET, DELETE, and UPDATE endpoints for posts with error handling

--- fastapi_crud/schema.py
+++ fastapi_crud/schema.py
@@ -1,9 +1,40 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,ConfigDict,EmailStr
+
+
+# reponse modele to get_all_posts
+class Response(BaseModel):
+    title:str
+    content:str
+    model_config = ConfigDict(from_attributes=True)
+
+
+
 #Add Pydantic model 'create_post' 
-class create_post(BaseModel):
+class Create_Post(BaseModel):
 
     title:str
     content:str
 
-class response_of_createpost(create_post):
+class Response_Of_Create_Post(Create_Post):
     id:int
+    model_config = ConfigDict(from_attributes=True)
+
+
+# pydantic model to respone a single post
+class Response_By_Id(Response_Of_Create_Post):
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+# To update a post with its path variable
+class new_post_data(BaseModel):
+    title:str
+    content:str
+    
+
+# response model of update_post
+class Update_Respons(BaseModel):
+    id:int
+    title:str
+    content:str
+    model_config=ConfigDict(from_attributes=True)
