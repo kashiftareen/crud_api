1b78bf07eb2dac745506f246e5e1df34f66d59e1
kashiftareen <kashif.tareen1999@gmail.com>
2025-08-07 22:54:06 +0500

    Add password hashing and verification functions using Passlib bcrypt

---
+++ fastapi_crud/utils.py
@@ -0,0 +1,9 @@
+# Hashes a plain password using the bcrypt algorithm for secure storage.
+from passlib.context import CryptContext
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
+def hash(password:str):
+    return pwd_context.hash(password)
+
+#Verifies that a plain password matches the previously hashed password using bcrypt.
+def verify(plain_password,hashed_password):
+    return pwd_context.verify(plain_password,hashed_password)
